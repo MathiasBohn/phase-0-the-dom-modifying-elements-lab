f4af4b45d3c3edb6e1df1131854af54ec13bdcd8
Mathias <mathiasbohndk@gmail.com>
2024-09-06 14:45:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main1 = document.getElementById("main")
+main1.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Mathias is the champion";
